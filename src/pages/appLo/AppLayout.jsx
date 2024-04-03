f2031088b7c2829c09bb72c98ca689b94e2453ee
afshin <afshinbakh1382@gmai.com>
2024-04-04 00:34:00 +0430

    main

---
+++ src/pages/appLo/AppLayout.jsx
@@ -0,0 +1,17 @@
+import Map from "../../components/map/Map";
+import Sidebar from "../../components/sidebar/Sidebar";
+import User from "../../components/user/User";
+
+import styles from "./AppLayout.module.css";
+
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+      <User />
+    </div>
+  );
+}
+
+export default AppLayout;
